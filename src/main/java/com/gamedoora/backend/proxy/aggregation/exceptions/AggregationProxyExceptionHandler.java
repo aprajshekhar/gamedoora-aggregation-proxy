9037994b1b3b0e3d027c02a27408b6ad939faa04
A.P.Rajshekhar <ap.rajshekhar@gmail.com>
2023-09-17 11:12:33 +0530

    Initial commit

---
+++ src/main/java/com/gamedoora/backend/proxy/aggregation/exceptions/AggregationProxyExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.gamedoora.backend.proxy.aggregation.exceptions;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestController
+public class AggregationProxyExceptionHandler extends ResponseEntityExceptionHandler {
+}
